fb2cbea27f0c8b7aa574bbd2e02696f211836b87
AppiahEnoch <prignutt@gmail.com>
2023-01-21 04:55:56 -1200

    33

--- AE/checkfiles.php
+++ AE/checkfiles.php
@@ -1,17 +1,21 @@
 <?php
 
-
-$directory = "./file/";
+$directory = "../file/";
 if(is_dir($directory)) {
-  $files = scandir($directory);
-  if(count($files) > 2) {
-    echo "Directory contains files.";
-  } else {
-    echo "Directory is empty.";
-  }
+    $files = scandir($directory);
+    if(count($files) > 2) {
+        echo "List of files in the directory: <br>";
+        foreach($files as $file) {
+            if($file != "." && $file != "..") {
+                echo $file . "<br>";
+            }
+        }
+    } else {
+        echo "Directory is empty.";
+    }
 } else {
-  echo "Not a valid directory.";
+    echo "Not a valid directory.";
 }
 
-echo "|||". $_SERVER['DOCUMENT_ROOT'];
+//echo "|||". $_SERVER['DOCUMENT_ROOT'];
 
